fe82236bee6cdc8b8cc577438f5cb079118110dc
Chinnatan <korn.chinnatan@gmail.com>
2026-03-01 23:22:31 +0700

    perf: add initial project configuration files and local database constants

--- lib/core/constant/local_db_const.dart
+++ lib/core/constant/local_db_const.dart
@@ -1,5 +1,5 @@
 class LocalDBConst {
-  static const String schema = '/local_db/pb_schema.json';
+  static const String schema = 'local_db/pb_schema.json';
 
   static const String users = 'users';
   static const String stores = 'stores';
